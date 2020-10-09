fb3c9f46ca9799de64cbf7e48c2db47786036c50
Bryce Moore <brycemooore@gmail.com>
2020-10-09 15:51:54 -0400

    Finished bank lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
 
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end 
+
+    def withdraw(withdrawal_amount)
+        self.balance -= withdrawal_amount
+    end 
+
+    def deposit(deposit_amount)
+        self.balance += deposit_amount
+    end 
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0
+    end 
+
+    def close_account
+        self.status = "closed"
+    end
 end
